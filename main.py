e4fcf6a0a28df7282bf92044d1c2bd0ebf569f57
salelder <sse34@cornell.edu>
2014-05-30 23:28:35 -0400

    Made some small changes
    Changes include:
    (1) fixed a syntaxical mistake; dot(x,x,x) should be dot(dot(x,x),x)
    (2) corrected omega to omega times small time step where an angle was
    needed
    (3) now plots the height of the tip of a physical pendulum, and the
    mathematically calculated period for comparison

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from numpy import *
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 def Rot1(t):
     """Returns a rotation matrix for axis 1 and finite angle t."""
-    return array([[1,0,0], [0,cos(t),-sin(t)], [0,sin(t),cos(t)]]);
+    return array([[1.,0.,0.], [0.,cos(t),-sin(t)], [0.,sin(t),cos(t)]]);
 def Rot2(t):
     """Returns a rotation matrix for axis 2 and finite angle t."""
-    return array([[cos(t),0,sin(t)], [0,1,0], [-sin(t),0,cos(t)]]);
+    return array([[cos(t),0.,sin(t)], [0.,1.,0.], [-sin(t),0.,cos(t)]]);
 def Rot3(t):
     """Returns a rotation matrix for axis 3 and finite angle t."""
-    return array([[cos(t),-sin(t),0], [sin(t),cos(t),0], [0,0,1]]);
-def Rotd(t1,t2,t3):
-    """Returns a rotation matrix for infinitesimal angles t1, t2, t3
+    return array([[cos(t),-sin(t),0.], [sin(t),cos(t),0.], [0.,0.,1.]]);
+def Rotd(t):
+    """Returns a rotation matrix for array of infinitesimal angles t1, t2, t3
     about axes 1, 2, 3, respectively."""
-    return array([[1,-t3,t2], [t3,1,-t1], [-t2,t1,1]]);
+    t1= t[0]; t2= t[1]; t3= t[2];    
+    return array([[1.,-t3,t2], [t3,1.,-t1], [-t2,t1,1.]]);
 
-def RotI(I, t1, t2, t3):
-    """."""
+def unit(v):
+    """Returns a unit vector as an array with direction given by array v."""
+    return v/linalg.norm(v)
         
 class RigidBody:
     """Encodes a rigid body and provides methods for applying moments to it.
@@ -28,13 +29,16 @@ class RigidBody:
         for the object about the same point that moments will be computed about.
         E.g., In the case of a pendulum, this is most likely the pivot point."""
         self.I11= I11; self.I22= I22; self.I33= I33; self.a1= a1; self.a2= a2; self.a3= a3;
-        self.I= array([[I11,0,0], [0,I22,0], [0,0,I33]]);
+        self.I= array([[I11,0.,0.], [0.,I22,0.], [0.,0.,I33]]);
         self.w= w;
         
-    def Rotate(self, R):
+    def rotate(self, R):
         """Rotates the rigid object according to the rotation matrix provided."""
-        self.a1= dot(R, self.a1); self.a2= dot(R, self.a2); self.a3= dot(R, self.a3);
-        self.I= dot(R, self.I, transpose(R));
+        self.a1= unit(dot(R, self.a1))
+        self.a2= unit(dot(R, self.a2))
+        self.a3= unit(dot(R, self.a3)) # rescale to unit vectors to help correct for errors
+        
+        self.I= dot(dot(R, self.I), R.transpose());
         
     def alpha(self, M):
         """Computes the angular acceleration of the object given the total
@@ -44,34 +48,31 @@ class RigidBody:
 
 L= 1.
 m= 1.
-t0= pi/4
+t0= pi/6
 g= 9.81
-a1= array([sin(t0),-cos(t0),0.]); a2= array([cos(t0),sin(t0),0.]); a3= array([0.,0.,1.])
-rod= RigidBody(0,m*L**2/3,m*L**2/3, a1,a2,a3, array([0.,0.,0.]))
+
+rod= RigidBody(0.,(m*L**2)/3,1.,
+    array([sin(t0),-cos(t0),0.]),array([cos(t0),sin(t0),0.]),array([0.,0.,1.]),
+    array([0.,0.,0.]))
 def Moment():
     """Return the total external moment on the rod defined above."""
-    return array([0.,0.,L*m*g*rod.a1[1]/rod.a2[0]/2.]);
-dt=.05; nframes= 500
-a1= []; a2= []; # these will store the values of a1 and a2 for time k
+    return array([0.,0.,-.5*L*m*g*rod.a1[0]]);
+
+time= []
+ypos= [] # of the tip of the pendulum    
+dt=.001
 k= 0
-while k < nframes:
-    a1.append(rod.a1[0]); a2.append(rod.a2[0]);
-    a= rod.alpha(Moment());
-    rod.w+= a*dt;
-    rod.a1= dot(Rot3(rod.w[2]), rod.a1);
-    k+= 1;
+while k < 5/dt:
+    time.append(k*dt)
+    ypos.append(rod.a1[1]) # y-component of principal axis 1    
     
-fig= plt.figure()
-ax= fig.add_subplot(111, xlim=(-1.5, 1.5), ylim=(-1.5,1.5))
-line, = ax.plot([],[],'b-')
+    a= rod.alpha(Moment())
+    rod.rotate(Rotd(rod.w*dt))
+    rod.w+= a*dt;
+    #print(rod.a1[0]) # x-component of the pendulum
+    k+= 1
 
-def animate(k):
-    line.set_data(array([0,a1[k][0]]), array([0,a1[k][1]]))  # update the data
-    return line,
-
-def init():
-    line.set_data(np.array([0,1]), np.array([0,0]))
-    return line,
-# must assign to something    
-a= animation.FuncAnimation(fig, animate, frames=arange(0,nframes), init_func=init, interval=1, blit=True)
+plt.plot(time, ypos)
+T= 2.*pi*sqrt(2.*L/(3.*g)) # the mathematically derived period
+plt.plot([T,T],[-1,0],'r--')
 plt.show()
